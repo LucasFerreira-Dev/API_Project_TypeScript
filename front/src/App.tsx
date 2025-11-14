d8dd4b29108a022a1e731fe93bd35ea5458b2e73
Lucas <lucasgabrieldasilvaferreira76@gmail.com>
2025-11-14 19:17:11 -0300

    configurando o css próprio da pag login

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import Login from "./pages/login";
+//import Login from "./pages/login";
+import LoginCadastrar from "./pages/loginCadastrar";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <LoginCadastrar />
     </div>
   );
 }
